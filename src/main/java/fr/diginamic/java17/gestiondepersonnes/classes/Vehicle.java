d0845f7de7842e45b073de8ca3445ffe8f829f2d
Garmi <fallgarmi83@yahoo.fr>
2022-07-03 14:57:39 +0200

    Ajout des classes Vehicle Voiture et VoiturePoste

---
+++ src/main/java/fr/diginamic/java17/gestiondepersonnes/classes/Vehicle.java
@@ -0,0 +1,17 @@
+package fr.diginamic.java17.gestiondepersonnes.classes;
+
+public abstract class Vehicle {
+	private String immatriculation;
+
+	// Constructor
+	public Vehicle(String immatriculation) {
+		super();
+		this.immatriculation = immatriculation;
+	}
+
+	// Getter
+	public String getImmatriculation() {
+		return immatriculation;
+	}
+
+}
---
+++ src/main/java/fr/diginamic/java17/gestiondepersonnes/classes/Voiture.java
@@ -0,0 +1,10 @@
+package fr.diginamic.java17.gestiondepersonnes.classes;
+
+public abstract class Voiture extends Vehicle {
+
+	public Voiture(String immatriculation) {
+		super(immatriculation);
+		// TODO Auto-generated constructor stub
+	}
+
+}
